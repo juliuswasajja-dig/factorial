84bcd97a90d22c649eff89543f4761977d6444ee
Joshua Makula <makulajosh@gmail.com>
2020-10-05 14:55:47 +0300

    first commit

---
+++ test.js
@@ -0,0 +1,12 @@
+const assert = require('assert');
+const factorial1 = require('./fa');
+
+
+describe ('factorial', ()=>{
+    
+    it('finding out whether the factorial of 3 is 6', () =>{
+        assert.equal(6, factorial1);
+    
+   
+})
+})
